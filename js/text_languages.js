33eeb98ea7428cf46e9bb8c1182b522f417a32be
TheDeiw <chehovskyi150@gmail.com>
2025-02-10 22:57:04 +0200

    Add-Language-Translation

---
+++ js/text_languages.js
@@ -0,0 +1,14 @@
+const translations = {
+	en: {
+		about_me: "About me",
+		games: "Games",
+		projects: "Projects",
+		download_games: "Download games"
+	},
+	ua: {
+		about_me: "Про мене",
+		games: "Ігри",
+		projects: "Проєкти",
+		download_games: "Завантажити ігри"
+	}
+};
---
+++ js/translation.js
@@ -0,0 +1,42 @@
+document.querySelectorAll('.translator__item').forEach(item => {
+	item.addEventListener('click', () => {
+		 // Знаходимо поточний вибраний елемент і прибираємо клас
+		 const selectedItem = document.querySelector('.translator__selected');
+		 if (selectedItem && selectedItem !== item) {
+			  selectedItem.classList.remove('translator__selected');
+		 }
+		 
+		 // Додаємо клас до натиснутого елемента
+		 item.classList.add('translator__selected');
+		 changeLanguage(item.id);
+	});
+});
+
+function changeLanguage(language) {
+	// Отримати всі елементи, які мають атрибут data-key
+	const elements = document.querySelectorAll("[data-key]");
+
+	// Пройти по кожному елементу та змінити текст на відповідний переклад
+	elements.forEach(element => {
+		 const key = element.getAttribute("data-key");
+		 if (translations[language][key]) {
+			  element.textContent = translations[language][key];
+		 }
+	});
+
+	 // Зміна класу стилю залежно від мови
+    const body = document.body; // або інший контейнер, де треба змінити стиль
+
+    if (language === 'en') {
+        body.classList.remove('ua-style');
+        body.classList.add('en-style');
+    } else if (language === 'ua') {
+        body.classList.remove('en-style');
+        body.classList.add('ua-style');
+    }
+}
+
+// Встановити мову за замовчуванням
+document.addEventListener("DOMContentLoaded", () => {
+	changeLanguage('en'); // Початкова мова
+});
