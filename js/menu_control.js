2385b8838a40357a81b75eb8ce80087ba6c40237
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-03-26 23:39:47 +0200

    Feature: Add "About me" section

--- js/menu_control.js
+++ js/menu_control.js
@@ -5,7 +5,7 @@
 // 	dropdownBtn.addEventListener("click", function () {
 // 		 // Отримуємо координати кнопки
 // 		 const rect = dropdownBtn.getBoundingClientRect();
-		 
+
 // 		 // Встановлюємо позицію меню поза nav
 // 		 dropdownMenu.style.position = "absolute";
 // 		 dropdownMenu.style.top = `${rect.bottom + window.scrollY}px`;
@@ -22,22 +22,31 @@
 // });
 
 document.addEventListener("DOMContentLoaded", function () {
-	const downloadButton = document.querySelector(".menu__link_downloader");
-	const gamesList = document.querySelector(".menu_games__list");
-	const dropdown = document.querySelector(".menu_games__drop-down");
+    const downloadButton = document.querySelector(".menu__link_downloader");
+    const gamesList = document.querySelector(".menu_games__list");
+    const dropdown = document.querySelector(".menu_games__drop-down");
 
-	if (downloadButton && gamesList) {
-		 downloadButton.addEventListener("click", function () {
-			  gamesList.classList.toggle("active");
-			  dropdown.classList.toggle("active");
-		 });
+    if (downloadButton && gamesList) {
+        downloadButton.addEventListener("click", function () {
+            gamesList.classList.toggle("active");
+            dropdown.classList.toggle("active");
+        });
 
-		 // Закриття меню при кліку поза ним
-		 document.addEventListener("click", function (event) {
-			  if (!downloadButton.contains(event.target) && !gamesList.contains(event.target)) {
-					gamesList.classList.remove("active");
-					dropdown.classList.remove("active");
-			  }
-		 });
-	}
+        // Закриття меню при кліку поза ним
+        document.addEventListener("click", function (event) {
+            if (!downloadButton.contains(event.target) && !gamesList.contains(event.target)) {
+                gamesList.classList.remove("active");
+                dropdown.classList.remove("active");
+            }
+        });
+    }
+});
+
+document.addEventListener("scroll", () => {
+    const header = document.querySelector(".header");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
--- js/text_languages.js
+++ js/text_languages.js
@@ -1,22 +1,28 @@
 const translations = {
-	en: {
-		about_me: "About me",
-		games: "Games",
-		projects: "Projects",
-		download_games: "Download games",
-		main_heading_begin: "I'm",
-		thedeiw: " TheDeiw",
-		main_heading_end: "Welcome to my site",
-		sub_heading: "Here you can see all my projects and download my games. I am glad to see you here"
-	},
-	ua: {
-		about_me: "Про мене",
-		games: "Ігри",
-		projects: "Проєкти",
-		download_games: "Завантажити ігри",
-		main_heading_begin: "Привіт! Я",
-		thedeiw: " TheDeiw",
-		main_heading_end: "Вітаю на моєму сайті",
-		sub_heading: "Тут ви можете побачити всі мої проєкти та завантажити мої ігри. Радий вас тут бачити"
-	}
+    en: {
+        about_me: "About me",
+        games: "Games",
+        projects: "Projects",
+        download_games: "Download games",
+        main_heading_begin: "I'm",
+        thedeiw: " TheDeiw",
+        main_heading_end: "Welcome to my site",
+        sub_heading: "Here you can see all my projects and download my games. I am glad to see you here",
+        my_name: "Chekhovskyi Dmytro",
+        small_description: "Junior Frontend developer, Unity developer, Photoshop editor",
+        city: "Lviv, Ukraine",
+    },
+    ua: {
+        about_me: "Про мене",
+        games: "Ігри",
+        projects: "Проєкти",
+        download_games: "Завантажити ігри",
+        main_heading_begin: "Привіт! Я",
+        thedeiw: " TheDeiw",
+        main_heading_end: "Вітаю на моєму сайті",
+        sub_heading: "Тут ви можете побачити всі мої проєкти та завантажити мої ігри. Радий вас тут бачити",
+        my_name: "Чеховський Дмитро",
+        small_description: "Junior Frontend розробник, Unity розробник, Photoshop редактор",
+        city: "Львів, Україна",
+    },
 };
