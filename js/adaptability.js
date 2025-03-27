2095d01baa21c5c2648f73b58f9efa1e0d22cf0a
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-03-27 21:05:54 +0200

    Feature: Add adaptation on section "About me"

--- js/adaptability.js
+++ js/adaptability.js
@@ -1,40 +1,55 @@
 const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (
-			isMobile.Android() ||
-			isMobile.BlackBerry() ||
-			isMobile.iOS() ||
-			isMobile.Opera() ||
-			isMobile.Windows());
-	}
-}
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+    },
+};
 
-if (isMobile.any()){
-	document.body.classList.add('_touch');
+if (isMobile.any()) {
+    document.body.classList.add("_touch");
 } else {
-	document.body.classList.add('_pc');
+    document.body.classList.add("_pc");
 }
 
-const iconMenu = document.querySelector('.header__menu_icon');
-if (iconMenu){
-	const iconBody = document.querySelector('.menu__list');
-	iconMenu.addEventListener("click", function(e){
-		iconMenu.classList.toggle('open');
-		iconBody.classList.toggle('active');
-	})
+const iconMenu = document.querySelector(".header__menu_icon");
+const iconBody = document.querySelector(".menu__list");
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle("open");
+        iconBody.classList.toggle("active");
+    });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth",
+            });
+            if (iconMenu) {
+                iconMenu.classList.toggle("open", false);
+                iconBody.classList.toggle("active", false);
+            }
+        }
+    });
+});
