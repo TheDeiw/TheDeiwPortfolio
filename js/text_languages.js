5e78be9d853f36ab7136061147ba42f473a57caa
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-04-04 00:49:35 +0300

    Feature: Add projects section

--- js/text_languages.js
+++ js/text_languages.js
@@ -21,6 +21,15 @@ const translations = {
         download_game: "Download game",
         fall_balls_description:
             "A small game in which you need to catch multi-colored balls so that they do not go out in eternal darkness.",
+        portfolio_website: "Portfolio website",
+        thedeiws_games_description: `I am interested in game development. Additionally, I want people to see my projects, so I am also practicing web development to 
+                                    create a website where I can showcase my games. This way, my website will also serve as a portfolio.`,
+        visit_website: "Visit website",
+        division_plus: "Division Plus",
+        coursework: "Data manager | Coursework project",
+        division_plus_description: `DivisionPlus is a WPF-based application designed to manage and visualize data about soldiers in a battalion.
+								It supports features like filtering, sorting, editing, and analyzing soldier details, making it ideal for organizing and accessing personnel data.`,
+        visit_repository: "Visit repository",
     },
     ua: {
         about_me: "Про мене",
@@ -33,16 +42,28 @@ const translations = {
         sub_heading: "Тут ви можете побачити всі мої проєкти та завантажити мої ігри. Радий вас тут бачити",
         my_name: "Чеховський Дмитро",
         small_description: `Junior Frontend розробник, Unity розробник, Photoshop редактор <br> Львів, Україна`,
-        card_description: `Привіт! Мене звати Дмитро. Відомий під ніком “TheDeiw”. Є студентом за спеціальністю “Інженерія програмного забезпечення”.<br>
-                            Захоплююсь розробкою ігор. У цій галузі спеціалізуюся на Unity та C#. Розробив кілька власних проєктів, де експериментую з ігровими механіками, фізикою та рівневим дизайном. Також знайомий із Unreal Engine, 3D-моделюванням та оптимізацією продуктивності.<br>
+        card_description: `<span style='font-weight:90'>Привіт! Мене звати Дмитро. Відомий під ніком “TheDeiw”. Є студентом за спеціальністю “Інженерія програмного забезпечення”.<br>
+                            Захоплююсь розробкою ігор. У цій галузі спеціалізуюся на Unity та C#. Розробив кілька власних проєктів, де експериментую з ігровими механіками, фізикою та 
+                            рівневим дизайном. Також знайомий із Unreal Engine, 3D-моделюванням та оптимізацією продуктивності.<br>
                             Також розбираюсь у Frontend-розробці. Створював різні сайти, зокрема TheDeiwGames.<br>
                             Обожнюю створювати та редагувати у Photoshop та працювати з дизайном.<br>
-                            Вітаю Вас на своєму портфоліо, приємно познайомитись!`,
+                            Вітаю Вас на своєму портфоліо, приємно познайомитись!</span>`,
         unity_game: "Гра на Unity",
         blocky_description:
             "<span style='font-weight:90'>Проста 3D головоломка з ортографічною камерою. Зробив по відео-уроку.<br>Ви граєте за червоний рямокутник, який треба визволити з цієї пастки.</span>",
         download_game: "Завантажити гру",
         fall_balls_description:
             "<span style='font-weight:90'>Невелика гра, в якій потрібно ловити кульки різного кольору, щоб вони не потрапили в вічну темряву.</span>",
+        portfolio_website: "Портфоліо вебсайт",
+        thedeiws_games_description: `<span style='font-weight:90'>Мене цікавить розробка ігор. Крім того, я хочу, щоб люди бачили мої проекти,
+                                        тому я також практикую веб-розробку, щоб створити веб-сайт, де я можу продемонструвати свої ігри. 
+                                        Таким чином, мій веб-сайт також буде служити портфоліо.</span>`,
+        visit_website: "Відвідати вебсайт",
+        division_plus: '<span class="ua-style">Дивізія +</span>',
+        coursework: "Менеджер даних | Курсовий проєкт",
+        division_plus_description: `<span style='font-weight:90'>Дивізія + — це програма на основі WPF, призначена для керування та візуалізації даних про солдатів у батальйоні. 
+                                    Підтримуються такі функції, як фільтрація, сортування, редагування та аналіз даних про солдатів, 
+                                    що робить програму ідеальною для впорядкування та доступу до даних персоналу.</span>`,
+        visit_repository: "Відвідати репозиторій",
     },
 };
